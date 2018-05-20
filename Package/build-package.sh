b340fca3db4c5591e477f78d6b812d934b1b7fa8
Pratik Patil <patil.pratik.r@gmail.com>
2018-05-20 23:22:48 +0530

    build version is automated using git tag

--- Package/build-package.sh
+++ Package/build-package.sh
@@ -4,9 +4,80 @@
 
 source ./Package.config
 
-echo ${PKG_NAME}
-echo ${BASE_VERSION} 
+TRUE=1
+FALSE=0
 
+RELEASE=FALSE
+BUILD_VERSION=""
+ADD_GIT_TAG=FALSE
+
+
+usage()
+{
+    echo "Build debian package. Make sure to have correct info in Package.config"
+    echo ""
+    echo "./build-package.sh"
+    echo "-h --help"
+    echo "--build=[release|dev]
+    echo "--tag=[tag]
+    echo ""
+}
+
+# validate arguments
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -b | --build)
+            if [ ${VALUE} = "release" ]; then
+              RELEASE=${TRUE}
+            elif [ ${VALUE} = "dev" ]; then
+              RELESE=${FALSE}
+            else
+              echo "Incorrect build type"
+              usage
+              exit
+            fi
+            ;;
+        -t | --tag)
+            if [ -z ${VALUE} ]; then 
+              GIT_TAG=${BASE_VERSION}
+            else
+              GIT_TAG=${VALUE}
+            fi
+              ADD_GIT_TAG=${TRUE}
+              echo "$GIT_TAG"
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Check for release build.
+if [ ${RELEASE} -eq ${TRUE} ]; then
+  echo "Building release"
+  BUILD_VERSION="${BASE_VERSION}.${BUILD_NO}"
+else
+  echo "building dev builds"
+  if [ ${ADD_GIT_TAG} -eq ${TRUE} ]; then
+    # set tag on git
+    echo "Adding tag in git"
+    git tag -a v${GIT_TAG} -m 'Version ${GIT_TAG}'
+    git push --tags
+  fi
+  
+  # get build version from git.
+  BUILD_VERSION=`git describe --long --tags --dirty --always`
+fi
 
 echo "Making Package directory for ${PKG_NAME}"
 if [ -d ${PKG_NAME} ]; then
@@ -26,4 +97,4 @@ cp -r Deliverable/DEBIAN ${PKG_NAME}
 
 dpkg-deb --build ${PKG_NAME}
 
-mv ${PKG_NAME}.deb ${PKG_NAME}_${BASE_VERSION}.deb
+mv ${PKG_NAME}.deb ${PKG_NAME}_${BUILD_VERSION}.deb
